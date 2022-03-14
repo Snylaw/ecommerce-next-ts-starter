4cd7e83954f2dccd3aa131a8a8c85daf3e091c8b
Rafaël Daigneau <rafael.daigneau@gmail.com>
2022-03-14 17:42:32 +0100

    [TS] Custom types

--- playground.ts
+++ playground.ts
@@ -1,8 +1,17 @@
 
+type Person = {
+    name: string
+    age: number
+}
 
 export default function play() {
     const name = "Rafael"
     const age = 23
+
+    const person: Person = {
+        name: "John",
+        age: 30
+    }
 
     function logPersonInfo(personName: string, personAge: number) {
         const info = `Name: ${personName}, age: ${personAge}`
@@ -10,6 +19,12 @@ export default function play() {
         return info
     }
 
-    logPersonInfo(name, age)
+    function logPersonInfo2(person: Person) {
+        const info = `Name: ${person.name}, age: ${person.age}`
+        console.log(info)
+        return info
+    }
 
+    logPersonInfo(name, age)
+    logPersonInfo2(person)
 }
