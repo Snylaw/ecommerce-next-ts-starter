7a99f7661bd47adf3b832a4b3993ea4312695864
Rafaël Daigneau <rafael.daigneau@gmail.com>
2022-03-14 16:26:29 +0100

    Typescript init

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+
+export default function Home() {
+
+  const message: string = "Hello World"
+
+  let a: AddEventListenerOptions;
+  let b: NodeJS.Process
+
+  let person: Person = {
+    name: "Rafael"
+  }
+
+  // console.log(b.browser)
+
+  return (
+    <div>
+      {message}
+    </div>
+  )
+}
