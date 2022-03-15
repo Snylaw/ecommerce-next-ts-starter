ddec665a315bd662ec1c4dfd1a9e896c9b4aacd9
Rafaël Daigneau <rafael.daigneau@gmail.com>
2022-03-15 19:56:04 +0100

    [TS] Extending Type

--- playground.ts
+++ playground.ts
@@ -13,20 +13,22 @@ interface AcademicPerson extends Person {
     publications: string[]
 }
 
+type Car = {
+    name: string
+}
+
+type RaceCar = {
+    speed: number
+} & Car & { mileage: number }
+
 export default function play() {
-    const person: AcademicPerson = {
-        name: "Rafael",
-        age: 23,
-        publications: ["1","2"]
+    const car: RaceCar = {
+        name: "Clicli",
+        speed: 160,
+        mileage: 160000,
     }
 
-    const person2: BusinessPerson = {
-        name: "Rafael",
-        age: 23,
-        salary: 1000
-    }
+    function logCar(car: Car) { console.log(car) }
 
-    function logPerson(person: Person) {}
-
-    logPerson(person2)
+    logCar(car)
 }
