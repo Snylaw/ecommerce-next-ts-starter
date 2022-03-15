8779a2251156a3edbd17b3e765008095624dbe38
Rafaël Daigneau <rafael.daigneau@gmail.com>
2022-03-15 16:52:54 +0100

    [TS] Narrowing

--- playground.ts
+++ playground.ts
@@ -1,52 +1,15 @@
 
-// interface Person {
-//     name: string
-//     age: number
-// }
-
-class Person {
-    name: string
-    age?: number
-
-    constructor(name: string, age: number) {
-        this.name = name
-        this.age = age
-    }
-}
-
-// interface PersonLoggerFn {
-//     (name: string, age: number): string
-// }
-
-// OU
-
-type PersonLoggerFn = (name: string, age?: number) => string     // Better for this 
 
 export default function play() {
-    const name: string = "Rafael"
-    const age: number = 23
 
-    const john: Person = {
-        name: "John"
+    const random = Math.random() > 0.5 ? "hello" : [1,2]
+
+    if (typeof random === "string") {
+        const upper = random.toUpperCase()
+    } else {
+        console.log(random)
     }
 
-    const logPersonInfo: PersonLoggerFn = (
-        personName: string, personAge: number = 0
-    ): string => {
-        const info = `Name: ${personName}, age: ${personAge}`
-        console.log(info)
-        return info
-    }
-
-    function logPersonInfo2(person: Person) {
-        const info = `Name: ${person.name}, age: ${person.age}`
-        console.log(info)
-        return info
-    }
-
-    const log: string = logPersonInfo(name)
-
-    const person = new Person("Rafael", 25)
-
-    logPersonInfo2(person)
+    console.log(random.length)
+    
 }
