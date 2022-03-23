79e7353742e14be80bce926db0790e4553ee2de7
Rafaël Daigneau <rafael.daigneau@gmail.com>
2022-03-23 17:49:41 +0100

    Infer get static props

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,26 @@
 import { useEffect } from "react"
-import play from "../playground"
+import type { InferGetStaticPropsType } from "next"
 
-export default function Home() {
+export async function getStaticProps() {
+  const products = [1,2,3]
 
-  useEffect(() => {
-    play();
-  }, [])
+  return {
+    props: {
+      products
+    },
+    revalidate: 4 * 60 * 60
+  }
+}
+
+export default function Home({
+  products
+}: InferGetStaticPropsType<typeof getStaticProps>) {
 
   return (
     <div>
-      Hello World
+      {
+        products
+      }
     </div>
   )
 }
