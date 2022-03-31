acbee642f5d08a6ece7fa85c1069ab65258334a2
Rafaël Daigneau <rafael.daigneau@gmail.com>
2022-03-31 17:02:22 +0200

    Normalize product

--- framework/shopify/product/get-all-products.ts
+++ framework/shopify/product/get-all-products.ts
@@ -1,6 +1,6 @@
 import getAllProductsQuery from "../utils/queries/get-all-products"
 import { ProductConnection } from "../schema"
-
+import { normalizeProduct } from "../utils/normalize"
 import fetchApi from "../utils/fetch-api"
 
 type ReturnType = {
@@ -10,9 +10,9 @@ type ReturnType = {
 const getAllProducts = async (): Promise<any> => {
     const { data } = await fetchApi<ReturnType>({query: getAllProductsQuery})
 
-    const products = data.products.edges.map(({ node: product }) => {
-        return product
-    })
+    const products = data.products.edges.map(({ node: product }) => 
+        normalizeProduct(product)
+    ) ?? []
 
     return products
 }
