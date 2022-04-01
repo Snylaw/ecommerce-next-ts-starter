67844a11f906618b7e45a711064cc54683c5b11c
Rafaël Daigneau <rafael.daigneau@gmail.com>
2022-04-01 16:53:19 +0200

    Framework specific configuration

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
-module.exports = {
+const { withFrameworkConfig } = require('./framework/common/config');
+
+module.exports = withFrameworkConfig({
   i18n: {
     locales: ["en-US", "es"],
     defaultLocale: "en-US"
   }
-}
+});
 
 console.log("next.config.js", JSON.stringify(module.exports, null, 2))
