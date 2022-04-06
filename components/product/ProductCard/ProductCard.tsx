404c3abdd322ac5f963163c09a96ca09df23e357
Rafaël Daigneau <rafael.daigneau@gmail.com>
2022-04-06 04:15:50 +0200

    Product info

--- components/product/ProductCard/ProductCard.tsx
+++ components/product/ProductCard/ProductCard.tsx
@@ -1,14 +1,40 @@
 import { Product } from "@common/types/product"
 import { FC } from "react"
+import Link from "next/link"
+import Image from "next/image"
 
 interface Props {
     product: Product
 }
 
+const placeholderImage = "/product-image-placeholder.svg"
+
 const ProductCard: FC<Props> = ({product}) => {
 
     return (
-        <div>{product.name}</div>
+        <Link href={`/products/${product.slug}`}>
+            <a>
+                <div>
+                    <h3>
+                        <span>
+                            {product.name}
+                        </span>
+                    </h3>
+                    <span>14 $</span>
+                    { product.images && (
+                        <Image 
+                            alt={product.name ?? "Product image"} 
+                            src={placeholderImage}
+                            width={540}
+                            height={540}
+                            quality="85"
+                            layout="responsive"
+                        />
+                    )
+                    }
+                </div>
+            </a>
+        </Link>
     )
 }
 
