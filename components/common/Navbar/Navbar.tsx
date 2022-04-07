ded4d23f644bd10d887270f47192477215caecb7
Rafaël Daigneau <rafael.daigneau@gmail.com>
2022-04-07 23:05:02 +0200

    Usernav

--- components/common/Navbar/Navbar.tsx
+++ components/common/Navbar/Navbar.tsx
@@ -2,6 +2,7 @@ import { Container } from "@components/ui"
 import { FC } from "react"
 import Link from "next/link"
 import s from "./Navbar.module.css"
+import { Usernav } from "@components/common"
 
 const Navbar: FC = () => {
 
@@ -36,6 +37,9 @@ const Navbar: FC = () => {
                             </a>
                         </Link>
                     </nav>
+                    <div className="flex flex-1 justify-end space-x-8">
+                        <Usernav />
+                    </div>
                 </div>
             </div>
         </Container>
