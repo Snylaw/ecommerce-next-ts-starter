d10c2293258f6d490033eb06b5c5d65f828386d8
Rafaël Daigneau <rafael.daigneau@gmail.com>
2022-04-19 18:10:07 +0200

    Sliding in keen slider

--- components/product/ProductSlider/ProductSlider.tsx
+++ components/product/ProductSlider/ProductSlider.tsx
@@ -1,22 +1,38 @@
 import React, { FC, Children, isValidElement } from 'react'
 import s from './ProductSlider.module.css'
+import { useKeenSlider } from "keen-slider/react"
 
 const ProductSlider: FC = ({children}) => {
 
+    const [sliderRef, _] = useKeenSlider<HTMLDivElement>({
+        initial: 0,
+        loop: true,
+        slideChanged(slider) {
+          console.log("Slide changed to: ", slider.track.details.rel)
+        },
+      })
+
     return (
         <div className={s.root}>
-            <div className="keen-slider h-full transition-opacity">
+            <div ref={sliderRef} className="keen-slider h-full transition-opacity">
                 { Children.map(children, child => {
 
                     if (isValidElement(child)) {
-                        // return {
-                        //     ...child,
-                        //     props: {
-                        //         ...child.props,
-                        //         className: "keen-slider__slide",
-                        //     }
-                        // }
-                        return React.cloneElement(child, { className: "keen-slider__slide" })
+                        return {
+                            ...child,
+                            props: {
+                                ...child.props,
+                                className: `${
+                                    child.props.className ? `${child.props.className}` : ""
+                                } keen-slider__slide` 
+                            }
+                        }
+
+                        // return React.cloneElement(child, { 
+                        //     className: `${
+                        //         child.props.className ? `${child.props.className}` : ""
+                        //     } keen-slider__slide` 
+                        // })
                     }
 
                     return child
