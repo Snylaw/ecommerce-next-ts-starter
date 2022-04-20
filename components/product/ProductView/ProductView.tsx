62e42cf798486c879a861fc9e724929bcb24437b
Rafaël Daigneau <rafael.daigneau@gmail.com>
2022-04-20 17:01:52 +0200

    Display variant options

--- components/product/ProductView/ProductView.tsx
+++ components/product/ProductView/ProductView.tsx
@@ -45,12 +45,19 @@ const ProductView: FC<Props> = ({ product }) => {
         </div>
         <div className={s.sidebar}>
           <section>
-            <div className="pb-4">
-              <h2 className="uppercase font-medium">Color</h2>
-              <div className="flex flex-row py-4">
-                Variant Options Here!
+            { product.options.map(option =>
+              <div key={product.id} className="pb-4">
+                <h2 className="uppercase font-medium">{option.displayName}</h2>
+                <div className="flex flex-row py-4">
+                  { option.values.map(value =>
+                      <div key={`${option.id}-${value.label}`} className='mb-4'>
+                        {value.label}
+                      </div>
+                  )}
+                </div>
               </div>
-            </div>
+            )}
+            
             <div className="pb-14 break-words w-full max-w-xl text-lg">
               {product.description}
             </div>
