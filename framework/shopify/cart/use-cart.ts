9fe4ef585a5a42ff3e96977134a81f281f58fe6c
Rafaël Daigneau <rafael.daigneau@gmail.com>
2022-04-29 03:32:44 +0200

    Call fetcher in use data

--- framework/shopify/cart/use-cart.ts
+++ framework/shopify/cart/use-cart.ts
@@ -3,14 +3,12 @@ import useCart from "@common/cart/use-cart"
 export default useCart
 
 export const handler = {
-    fetcherOptions: {
-        query: ""
+    fetchOptions: {
+        query: "query { hello }"
     },
-    fetcher() {
-        console.log("Not Called")
-        return {
-            data: "cart ready"
-        }
+    async fetcher({fetch, options}: any) {
+        const data = await fetch({...options})
+        return { data }
     },
     useHook: ({useData}: any) => {
         const data = useData()
