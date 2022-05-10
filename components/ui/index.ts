ca64251e3d7525239d16ca2390f67f3411fe8967
Rafaël Daigneau <rafael.daigneau@gmail.com>
2022-05-10 04:51:15 +0200

    Loading Dots + Default variant

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -5,3 +5,4 @@ export { default as Container } from './Container';
 export { default as Marquee } from './Marquee';
 export { default as Sidebar } from './Sidebar';
 export { default as Button } from './Button';
+export { default as LoadingDots } from './LoadingDots';
